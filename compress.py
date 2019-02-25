74e94c3d90698990e7963c5bf9ce97fc04945766
Eugene <supernovaprotocol@gmail.com>
2019-02-25 18:43:17 +0700

    Subsampling faster implementation

--- compress.py
+++ compress.py
@@ -2,24 +2,39 @@ from PIL import Image
 import numpy as np
 
 
-def slow_average_band(band, block_size=3):
+def block_view(a, block_size):
+    height = a.shape[0]
+    width = a.shape[1]
+    assert width % block_size == 0
+    assert height % block_size == 0
+
+    new_height = round(a.shape[0] // block_size)
+    new_width = round(a.shape[1] // block_size)
+
+    blocks = np.zeros((new_height, new_width, block_size, block_size), dtype=np.int)
+
+    tmp_height = int(width * height / block_size)
+    a = a.reshape((tmp_height, block_size))
+    stride = width // block_size
+
+    for j in range(stride):
+        blocks_column = a[j::stride]
+
+        for y in range(0, new_height):
+            i = y * block_size
+            blocks[y, j, :] = blocks_column[i:i + block_size]
+
+    return blocks.reshape((new_height, new_width, block_size, block_size))
+
+
+def average_band(band, block_size=3):
     pixels = np.array(list(band.getdata()))
 
     pixels = pixels.reshape((band.height, band.width))
 
-    new_height = round(band.height / block_size)
-    new_width = round(band.width / block_size)
+    blocks_matrix = block_view(pixels, block_size)
 
-    res = np.zeros((new_height, new_width))
-
-    for y in range(new_height):
-        for x in range(new_width):
-            i = y * block_size
-            j = x * block_size
-
-            res[y, x] = np.mean(pixels[i:i + block_size, j:j + block_size])
-
-    return res
+    return np.mean(blocks_matrix, axis=(2, 3))
 
 
 def compress(input_fname, output_fname):
@@ -27,9 +42,9 @@ def compress(input_fname, output_fname):
 
     y, cb, cr = im.split()
 
-    block_size = 3
-    mean_cb = slow_average_band(cb, block_size=block_size)
-    mean_cr = slow_average_band(cr, block_size=block_size)
+    block_size = 4
+    mean_cb = average_band(cb, block_size=block_size)
+    mean_cr = average_band(cr, block_size=block_size)
 
     import json
 
